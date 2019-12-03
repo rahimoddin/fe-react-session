6e3cfadd5cee1c1027778cec5f076fdcf121ee01
Rahim <rahimoddin.naikwade@isobar.com>
2019-12-03 14:59:38 +0530

    React training day-1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const element = document.createElement('div')
+// const txtELement = document.createTextNode('Some string for text node.')
+
+// element.appendChild(txtELement)
+
+// document.getElementById('root').appendChild(element)
+
+// const childElement = React.createElement('h2', {}, 'I am heand')
+
+// const element = React.createElement('div', {id: 'container', className: 'container'}, 'Some text node for react element')
+
+const element = <div className="container"><h2>I am  heading</h2><div className="child">I am child</div></div>
+
+ReactDOM.render(element, document.getElementById('root'))
+
+
+
